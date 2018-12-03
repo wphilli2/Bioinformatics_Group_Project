ed32068f88d25bfa6878b96bb61d683c7c68de9b
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-03 14:12:21 -0500

    Update hmmbuild.sh

--- Results_files/scripts/hmmbuild.sh
+++ Results_files/scripts/hmmbuild.sh
@@ -1,3 +1,7 @@
+##Using these alignments, construct 6 HMM protein models using hmmbuild
+##Build HMM protein models using HMMbuild
+#hmmbuild.sh
+
 for g in $(ls Bioinformatics_Group_Project/Results_files/*.align); do
 /afs/nd.edu/user29/jwharto1/local/bin/hmmbuild ${g%.align}.hmm $g
 done
