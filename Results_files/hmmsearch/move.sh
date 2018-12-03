53fb3193edfd4f5723eee0fe672325e4a413ea83
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-03 13:10:05 -0500

    Delete move.sh

--- Results_files/hmmsearch/move.sh
+++
@@ -1,5 +0,0 @@
-for file in $@
-do
-scp wphilli2@remote105.helios.nd.edu:/afs/nd.edu/user24/wphilli2/local/bin/Projectstuff/$file /Users/willphillips/Desktop/Introduction\ to\ Biocomputing/Bioinformatics/Project/Bioinformatics_Group_Project/Results_files/hmmsearch
-done
-
