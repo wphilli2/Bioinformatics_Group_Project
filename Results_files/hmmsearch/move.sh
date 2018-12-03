b2e2c11209a371023f20b4e13e5a631642ceb753
Will Phillips <wphilli2@nd.edu>
2018-12-03 13:09:25 -0500

    individual tables

---
+++ Results_files/hmmsearch/move.sh
@@ -0,0 +1,5 @@
+for file in $@
+do
+scp wphilli2@remote105.helios.nd.edu:/afs/nd.edu/user24/wphilli2/local/bin/Projectstuff/$file /Users/willphillips/Desktop/Introduction\ to\ Biocomputing/Bioinformatics/Project/Bioinformatics_Group_Project/Results_files/hmmsearch
+done
+
