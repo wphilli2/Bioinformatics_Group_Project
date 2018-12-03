0fa90e873ddeb666c365bc76be9b1f81b28bc1bc
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-03 10:39:09 -0500

    Update primate_script.sh

--- Results_files/ShortAns2/primate_script.sh
+++ Results_files/ShortAns2/primate_script.sh
@@ -2,7 +2,7 @@
 ## Description: The loop assigns a file name by echoing the file to be searched and the profile searched against. It then performs an hmmsearch on the 
 ## file and outputs the data to a file with the same name as the file and an additional ".hits" at the end. PtPn5_primate_profile.hmm (the protein profile for Ptpn5
 ##    created                                                        
-## Usage: bash primate_loop.sh <RNAseq file to be searched> <hmm profile to search against>
+## Usage: bash primate_script.sh <RNAseq file to be searched> <hmm profile to search against>
 
 
 filename=$(echo $1_$2)
