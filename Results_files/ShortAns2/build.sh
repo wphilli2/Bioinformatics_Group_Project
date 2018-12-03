ed3c917acabd29adf5a85dba8ededbc9aa13225c
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-03 10:38:43 -0500

    Update build.sh

--- Results_files/ShortAns2/build.sh
+++ Results_files/ShortAns2/build.sh
@@ -1,3 +1,6 @@
-##Usage: bash build.sh <input alignment file> <name of output file>
+## Usage: bash build.sh <input alignment file> <name of output file>
+## Description:	Builds an HMM from the input alignment file and	saves the model	to the output file
+
+
 /afs/nd.edu/user24/wphilli2/local/bin/hmmer/hmmbuild $2 $1
 
