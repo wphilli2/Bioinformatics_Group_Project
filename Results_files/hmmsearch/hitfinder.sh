28f841eacab63db89d1b996f38ff24ddd3bbcf9a
Will Phillips <wphilli2@nd.edu>
2018-12-07 14:14:11 -0500

    new script for searches

--- Results_files/hmmsearch/hitfinder.sh
+++ Results_files/hmmsearch/hitfinder.sh
@@ -1,16 +1,13 @@
 for x in $@
 do
-	cd $x
-	echo $x >> ../Controlhits.txt
-	echo $x >> ../Obesityhits.txt
-	for file in *Con*.hits
-		do 
-		grep -v "#" $file | wc -l >> ../Controlhits.txt
-		done
-	for file in *Obes*.hits
-		do
-		grep -v "#" $file | wc -l >>../Obesityhits.txt
-		done
-	cd ..
+        cd $x
+        A=$(grep -v "#" *Control1*.hits | wc -l)
+        echo $x.C1 $A >> ../modifiedhits.txt
+        B=$(grep -v "#" *Control2*.hits | wc -l)
+        echo $x.C2 $B >> ../modifiedhits.txt
+        C=$(grep -v "#" *Obese1*.hits | wc -l)
+        echo $x.O1 $C >> ../modifiedhits.txt
+        D=$(grep -v "#" *Obese2*.hits | wc -l)
+        echo $x.O2 $D >> ../modifiedhits.txt
+        cd ..
 done
-
