061720d30a3e3f777b0fd84af19aa6f9db227365
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-07 21:19:53 -0500

    Delete hmmsearch.sh

--- Results_files/scripts/hmmsearch.sh
+++
@@ -1,8 +0,0 @@
-##Finally, search all 4 translated "RNAseq files" for each of the 6 HMM protein models using hmmsearch
-##Do a hmmsearch
-
-for i in $(ls Bioinformatics_Group_Project/Results_files/*.hmm); do
-/afs/nd.edu/user29/jwharto1/local/bin/hmmsearch $i ~/Bioinformatics_Group_Project/uniquetranscripts.fasta;
-done
-#Saved search results in search.txt
-bash hmmsearch.sh > search.txt
