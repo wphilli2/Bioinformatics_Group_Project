0e6c36f08f0791c6d73191c511226110fc3d7277
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-07 22:07:54 -0500

    Update muscle.sh

--- Results_files/Protein_alignments/muscle.sh
+++ Results_files/Protein_alignments/muscle.sh
@@ -1,3 +1,11 @@
+###Build a HMM for each of the 6 transcript proteins and search the 4 translated "RNAseq files"
+
+
+##For each of the 6 transcripts make a muscle alignment from your downloaded protein sequences
+##Write script for alignment of protein transcripts
+#muscle.sh
+
+
 for f in $(ls Bioinformatics_Group_Project/Results_files/*.fasta); do
   muscle -in $f -out ${f%.fasta}.align;
 done
