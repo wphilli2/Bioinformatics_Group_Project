31ee5842cd08b59d8c85aff37a5c68b7205ab167
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-07 22:17:24 -0500

    Update hitfinder.sh

--- Results_files/scripts/hitfinder.sh
+++ Results_files/scripts/hitfinder.sh
@@ -1,3 +1,12 @@
+## Location: This script should be run in the directory containing the directories with searched hmm files for each protein. In this case,
+##     the script is run in hmmsearch/. 
+## Description: The script loops through all directories passed as an argument. It moves to the directory and sets a variable (A, B, C, or D)
+##     that stores a line count from all Control1, Control2, Obese1, or Obese2 files that have had all lines containing "#" removed. 
+##     The script then echoes the folder (the name of the protein), the category (Control1, Control2, Obese1, or Obese2), and the variable
+##     for that part of the loop. The echo output is appended to a file in hmmsearch/ (or the directory directly above) called hitcounts.txt
+##     Finally, the script moves back up one level and can start the loop over.
+## Usage: bash hitfinder.sh <all directories that you want to search through>
+
 for x in $@
 do
        cd $x
