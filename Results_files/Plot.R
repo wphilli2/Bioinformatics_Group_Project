6965117a6e3c7f327011c2f822a0f0854a428d27
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-07 21:49:26 -0500

    Update Plot.R

--- Results_files/Plot.R
+++ Results_files/Plot.R
@@ -1,3 +1,8 @@
+## Description: This R script sets hitcounts.txt (containing the protein name, the category (control 1 or 2 and obese 1 or 2) 
+##         and the number of hits from the hmmsearch as an object ("data"). Then it sets objects for each of the variable (protein, type,
+##         and hit number) and plots them in a bar graph using ggplot. The bars are colored by category.
+
+
 data=read.table("hitcounts.txt")
 data
 library(ggplot2)
