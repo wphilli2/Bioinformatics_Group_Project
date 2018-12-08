e6afeba7e783d85069b6d3e40309d5da650a62b2
Will Phillips <wphilli2@nd.edu>
2018-12-07 20:47:18 -0500

    Plot

--- Results_files/hmmsearch/hitfinder.sh
+++ Results_files/hmmsearch/hitfinder.sh
@@ -1,13 +1,14 @@
 for x in $@
 do
-        cd $x
+       cd $x
+        y=$(echo $x | sed -E 's/^([A-Z][a-z][a-z]).*\//\1/')
         A=$(grep -v "#" *Control1*.hits | wc -l)
-        echo $x.C1 $A >> ../modifiedhits.txt
+        echo $y.C1 $x Control $A >> ../hitcounts.txt
         B=$(grep -v "#" *Control2*.hits | wc -l)
-        echo $x.C2 $B >> ../modifiedhits.txt
+        echo $y.C2 $x Control $B >> ../hitcounts.txt
         C=$(grep -v "#" *Obese1*.hits | wc -l)
-        echo $x.O1 $C >> ../modifiedhits.txt
+        echo $y.O1 $x Obese $C >> ../hitcounts.txt
         D=$(grep -v "#" *Obese2*.hits | wc -l)
-        echo $x.O2 $D >> ../modifiedhits.txt
+        echo $y.O2 $x Obese $D >> ../hitcounts.txt
         cd ..
 done
