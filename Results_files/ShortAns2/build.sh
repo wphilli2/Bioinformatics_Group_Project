0d9e379bbf8d04c1e7372dd220ce832b41dd36db
wphilli2 <43612074+wphilli2@users.noreply.github.com>
2018-12-07 21:28:14 -0500

    Update build.sh

--- Results_files/ShortAns2/build.sh
+++ Results_files/ShortAns2/build.sh
@@ -2,5 +2,5 @@
 ## Description:	Builds an HMM from the input alignment file and	saves the model	to the output file
 
 
-/afs/nd.edu/user24/wphilli2/local/bin/hmmer/hmmbuild $2 $1
+<path to hmmbuild on remote machine> $2 $1
 
